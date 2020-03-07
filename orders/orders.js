9b611185ddd05b322d7802cc93f8adc4d44abcc5
dongrerohan421 <dongrerohan421@gmail.com>
2020-03-07 13:51:24 -0500

    create order

--- orders/orders.js
+++ orders/orders.js
@@ -11,15 +11,38 @@ const bodyParser = require("body-parser");
 app.use(bodyParser.json()); //allows application to receive JSON data through request
 
 mongoose.connect("mongodb+srv://kali-denali:Axcvbn5@booksservice-ewcap.mongodb.net/test?retryWrites=true&w=majority", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-},
-(err) => {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log("Connected to the mongodb - Orders service!");
-    }
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    },
+    (err) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log("Connected to the mongodb - Orders service!");
+        }
+    });
+
+//Load Model
+require("./Order");
+const Order = mongoose.model("Order");
+
+//Create new order
+app.post("/order", (req, res) => {
+    var newOrder = {
+        customerID: mongoose.Types.ObjectId(req.body.customerID),
+        bookID: mongoose.Types.ObjectId(req.body.bookID),
+        initialDate: req.body.initialDate,
+        deliveryDate: req.body.deliveryDate
+    };
+
+    var order = new Order(newOrder);
+    order.save().then(() => {
+        res.send("New order created!");
+    }).catch((err) => {
+        if (err) {
+            throw err;
+        }
+    });
 });
 
 var listener = app.listen(7777, () => {
